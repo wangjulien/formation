1b0577dd9f8bc5d24a92e384e3c09b6f0cb58175
wangjiliang <wangjiliang@gmail.com>
2018-01-03 17:03:58 +0100

    Application livre with javadoc

--- src/main/java/dao/IDaoConseiller.java
+++ src/main/java/dao/IDaoConseiller.java
@@ -13,24 +13,8 @@ import entity.Employee;
  */
 public interface IDaoConseiller extends IDaoObjet<Conseiller> {
 
-	/**
-	 * Renvoi liste des conseillers de l'agence dont l'identifiant est passe en
-	 * parametre
-	 * 
-	 * @param idAgence
-	 * @return List ou null
-	 */
+
 	public List<Conseiller> selectAllByGerantId(Long idGerant) throws DaoException;
 
-	/**
-	 * Renvoie le Employee dont le login et le mode de passe sont fourni en
-	 * parametre, null sinon
-	 * 
-	 * @param login
-	 * @param psw
-	 * @return Employee ou null
-	 * @throws DaoException
-	 *             la requete a echouee
-	 */
 	public Employee selectEmployeeByLogin(String login, String psw) throws DaoException;
 }
