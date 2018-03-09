b7347a3b7f2191cba0d28ccca682c56dec6d7cd5
Jiliang WANG <wangjiliang@gmail.com>
2018-03-09 11:43:29 +0100

    change place of workspace

--- src/main/java/org/formation/proxibanque/webservice/IConseillerWsFacade.java
+++ src/main/java/org/formation/proxibanque/webservice/IConseillerWsFacade.java
@@ -1,14 +1,12 @@
 package org.formation.proxibanque.webservice;
 
-import javax.ws.rs.Produces;
-
 /**
  * Interface Web Service REST pour conseiller a developer
  * 
  * @author JW
  *
  */
-@Produces({ "application/xml", "application/json" })
+//@Produces({ "application/xml", "application/json" })
 public interface IConseillerWsFacade {
 	
 	
